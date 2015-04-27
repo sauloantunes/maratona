fecdf8d86151d53e81cebacab12a75d839955992
Saulo Antunes <sauloantuness@gmail.com>
2015-04-27 19:29:53 -0300

    string problem

---
+++ uri2/1024.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+int main(){
+	int n;
+	string s;
+
+	cin >> n;
+	cin.get();
+
+	while(n--){	
+		getline(cin, s);
+
+		for (int i = 0; i < s.size(); ++i){
+			if(s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z')
+				s[i] += 3;
+		}
+
+
+		reverse(s.begin(), s.end());
+
+		for (int i = s.size()/2; i < s.size(); ++i){
+			s[i] -= 1;
+		}
+
+
+		cout << s << endl;
+	}
+}
