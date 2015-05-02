76acda04cea071f759a928bdec62ff40fb8ec997
Saulo Antunes <sauloantuness@gmail.com>
2015-05-02 12:07:48 -0300

    Update 1022.cpp

--- uri2/1022.cpp
+++ uri2/1022.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
 #include <cstdio>
 #include <cmath>
-
+ 
 using namespace std;
-
+ 
 int mdc (int a, int b){
-	return b ? mdc(b, a % b) : abs(a);
+    return b ? mdc(b, a % b) : abs(a);
 }
-
+ 
 int main(){
-	int n1,d1,n2,d2,n,a,b;
-	char c;
+    int n1,d1,n2,d2,n,a,b;
+    char op;
+ 
+    cin >> n;
+ 
+    while(n--){
+        scanf("%d / %d %c %d / %d", &n1, &d1, &op, &n2, &d2);
+         
+        switch(op){
+            case '+' : a = n1*d2 + n2*d1;
+                       b = d1*d2;
+                       break;
+ 
+            case '-' : a = n1*d2 - n2*d1;
+                       b = d1*d2;
+                       break;
+                                  
+            case '*' : a = n1*n2;
+                       b = d1*d2;
+                       break;
+ 
+            case '/' : a = n1*d2;
+                       b = d1*n2;
+                       break;
+                                    
+        }
+ 
+        int m = mdc(a,b);
 
-	cin >> n;
-
-	while(n--){
-		scanf("%D / %d %c %d / %d", &n1, &d1, &c, &n2, &d2);
-		
-		switch(c){
-			case '+' : a = n1*d2 + n2*d1;
-					   b = d1*d2;
-					   break;
-
-			case '-' : a = n1*d2 - n2*d1;
-					   b = d1*d2;
-					   break;
-					   			 
-			case '*' : a = n1*n2;
-					   b = d1*d2;
-					   break;
-
-			case '/' : a = n1*d2;
-					   b = n2*d1;
-					   break;
-					   			   
-		}
-
-		int m = mdc(a,b);
-
-		cout << a << "/" << b << " = " << a/m << "/" << b/m << endl;
-	}
+        cout << a << "/" << b << " = " << a/m << "/" << b/m << endl;
+    }
 }
