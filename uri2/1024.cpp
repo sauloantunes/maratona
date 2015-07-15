4bd359011cf7c8de34013a078d659502e32c74e7
Saulo Antunes <sauloantuness@gmail.com>
2015-07-15 13:32:17 -0300

    cleaning

--- uri2/1024.cpp
+++ uri2/1024.cpp
@@ -15,18 +15,14 @@ int main(){
 	while(n--){	
 		getline(cin, s);
 
-		for (int i = 0; i < s.size(); ++i){
+		for (int i = 0; i < s.size(); ++i)
 			if(s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z')
 				s[i] += 3;
-		}
-
 
 		reverse(s.begin(), s.end());
 
-		for (int i = s.size()/2; i < s.size(); ++i){
+		for (int i = s.size()/2; i < s.size(); ++i)
 			s[i] -= 1;
-		}
-
 
 		cout << s << endl;
 	}
